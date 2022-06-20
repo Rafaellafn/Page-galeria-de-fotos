f7106585b8b07ed2328c882a383668f7459ac97c
rafaellafn <rafaferreiraneta@gmail.com>
2022-06-20 20:16:54 +0200

    Adicionando evento de click

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const btns = document.querySelectorAll('.btn')
+const btns = document.querySelectorAll(".btn");
 
 btns.forEach(btn => {
-  btn.addEventListener('click',() => {
-    console.log("clicado"); 
-  })
-})
+  btn.addEventListener("click",(e) => {
+    makeActiveBtn(e);
+  });
+});
+
+const makeActiveBtn = (event) => {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  event.target.classList.add("active");
+};
